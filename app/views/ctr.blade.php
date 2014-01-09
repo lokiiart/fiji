1b2ced1b4ab2163740a60e75fb8d460393e36c40
sevendoors <seven5272@gmail.com>
2014-01-09 10:12:59 +0800

    测试

---
+++ app/views/ctr.blade.php
@@ -0,0 +1,4 @@
+@extends('layout')
+@section('content')
+	控制器
+@stop
---
+++ app/views/layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <title></title>
+</head>
+<body>
+        <h1>测试学习</h1>
+		@yield('content')
+</body>
+</html>
